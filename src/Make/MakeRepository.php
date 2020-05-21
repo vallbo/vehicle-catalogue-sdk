d35b93b5a0c0cce663de3e500aa6ce9b2d2c4d7e
Jan Král <jan.kral@carvago.com>
2020-05-21 15:46:47 +0200

    [TEAS-211][fix] remove uuids

--- src/Make/MakeRepository.php
+++ src/Make/MakeRepository.php
@@ -8,7 +8,7 @@ use Carvago\VehicleCatalogue\SDK\AbstractExternalRepository;
 use Carvago\VehicleCatalogue\SDK\VehicleCatalogueClient;
 
 /**
- * @method array<Make> findAll();
+ * @method Make[] findAll();
  */
 class MakeRepository extends AbstractExternalRepository
 {
--- src/Transmission/TransmissionRepository.php
+++ src/Transmission/TransmissionRepository.php
@@ -8,7 +8,7 @@ use Carvago\VehicleCatalogue\SDK\AbstractExternalRepository;
 use Carvago\VehicleCatalogue\SDK\VehicleCatalogueClient;
 
 /**
- * @method array<Transmission> findAll();
+ * @method Transmission[] findAll();
  */
 class TransmissionRepository extends AbstractExternalRepository
 {
